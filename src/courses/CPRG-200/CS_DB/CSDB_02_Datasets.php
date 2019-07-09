8e89fa97f806f39a76d7025ffb995a3472a9b568
James Defant <james.defant@edu.sait.ca>
2019-07-09 17:04:26 -0600

    Update CSDB_02_Datasets.php

--- src/courses/CPRG-200/CS_DB/CSDB_02_Datasets.php
+++ src/courses/CPRG-200/CS_DB/CSDB_02_Datasets.php
@@ -2,7 +2,7 @@
 
 namespace J\ClassNotes {
 
-  class Template extends Page
+  class CSDB_02_Datasets extends Page
   {
 
     /*-----------------------------------------------------------------*/
@@ -29,7 +29,21 @@ MAINHEADING;
     {
       $returnValue = '
 <h2>Intro</h2>
-<p>To add a database file </p>
+<p>To add a database file to a VS project:</p>
+<ol>
+  <li>
+    <kbd>Right-click</kbd> the current project in the <kbd>Solution Explorer</kbd> and choose 
+    <kbd>Add</kbd> => <kbd>Existing Item...</kbd> and choose the .mdf file you would like to import
+  </li>
+  <li>In the <kbd>Data Sources</kbd> panel, select <kbd>Add New Data Source...</kbd></li>
+  <li>Select <kbd>Database</kbd> and click <kbd>Next ></kbd></li>
+  <li>Select <kbd>Dataset</kbd> and click <kbd>Next ></kbd></li>
+  <li>Click the <kbd>New Connection...</kbd> button and choose the Data source by clicking <kbd>Change...</kbd></li>
+  <li>Choose <kbd>Microsoft SQL Server Database File</kbd> and click <kbd>OK</kbd></li>
+  <li>Choose the Database file name by clicking <kbd>Browse...</kbd></li>
+  <li>Navigate to the new file that was added to the project and click <kbd>Open</kbd></li>
+  <li>Click <kbd>OK</kbd> to add the database</li>
+</ol>
 
 
 ';

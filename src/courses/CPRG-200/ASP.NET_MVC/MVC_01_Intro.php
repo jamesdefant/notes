91f60c6161eb7e551a4ebe127f2109146c3b4e19
James Defant <james.defant@edu.sait.ca>
2019-07-23 13:27:15 -0600

    Update MVC_01_Intro.php

--- src/courses/CPRG-200/ASP.NET_MVC/MVC_01_Intro.php
+++ src/courses/CPRG-200/ASP.NET_MVC/MVC_01_Intro.php
@@ -48,6 +48,59 @@ MAINHEADING;
 <p>ASP.NET MVC is <b>Open Source</b> (can access and/or modify source code of system components)</p>
 <hr/>
 
+<h2>Create an MVC App</h2>
+<p>Start with the <b>Model</b> or the <b>Controller</b></p>
+<ol>
+  <li>Create an ASP.NET application and select the MVC checkbox</li>
+  <li>Right-click the <b>Controllers</b> folder and create a new controller</li>
+  <ul>
+    <li>This creates a new controller with a route set up in the <code>App_Start</code> -> <code>RouteConfig.cs</code></li>
+  </ul>
+  <li><b>Right-click</b> anywhere inside the route method and choose <b>Add View</b> to create a View</li>
+  <ul>
+    <li>Keep all and <b>only</b> presentation logic in the View</li>
+  </ul>
+  <li>A default View is created in the <b>Views</b> folder with a <code>.cshtml</code> extension</li>
+</ol>
+
+<h2>ViewBag</h2>
+<p>A <code>ViewBag</code> is a container that is dynamically typed so that you may add any property to it - <b>Keep it small</b></p>
+<pre><code>
+// Adding data to the ViewBag in the controller
+ViewBar.Title = "My New Page";
+
+// Accessing date from the ViewBag in a view
+&lt;Title&gt;@ViewBag.Title&lt;/Title&gt;
+</code></pre>
+
+<h3>Conditional Statements</h3>
+<pre><code>
+@if(ViewBag.ProductCount == 0) {
+  @:Out of stock
+} else if(ViewBag.ProductCount <= 5) {
+  @:Low stock
+} else {
+  @ViewBag.ProductCount
+}
+</code></pre>
+<pre><code>
+@switch((int)ViewBag.ProductCount)
+{
+  case 0:
+    @:Out of stock
+    break;
+    
+  case 1:
+    @:Low stock (@ViewBag.ProductCount)
+    break;
+
+  default:
+    @ViewBag.ProductCount
+    break;
+
+} 
+</code></pre>
+
 
 
 

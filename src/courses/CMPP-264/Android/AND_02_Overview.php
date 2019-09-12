83e9867cc27048d7ebec4925297bbe47cb18d799
James Defant <james.defant@gmail.com>
2019-09-12 14:54:46 -0600

    Update AND_02_Overview.php

--- src/courses/CMPP-264/Android/AND_02_Overview.php
+++ src/courses/CMPP-264/Android/AND_02_Overview.php
@@ -28,7 +28,43 @@ MAINHEADING;
     public function getContent() : string
     {
       $returnValue = '
-<h2>Intro</h2>
+<h2>Create an icon</h2>
+<p>You can create either a bitmapped icon set or a vector bassed icon set</p>
+<ol>
+  <li>
+    Navigate to <kbd>File</kbd> => <kbd>New</kbd>
+    <ul>either
+      <li>
+        => <kbd>Image Asset</kbd> - Create a bitmapped icon set
+        <ul>
+          <li>Load a picture</li>
+        </ul>
+      </li>
+      <li> => <kbd>Vector Asset</kbd> - Create a vector bassed icon set</li>
+    </ul>
+  </li>
+  
+</ol>
+
+<h2>MainActivity</h2>
+<pre><code>
+package com.jamesdefant.day7exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+</code></pre>
+<p>This is the generated main hub. It overrides the <code>onCreate()</code> method</p>
 
 
 ';

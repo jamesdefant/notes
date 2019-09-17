9b9bc8bdb32789bc2d744f28cbcb1eb27a4c0360
James Defant <james.defant@gmail.com>
2019-09-17 15:02:32 -0600

    Update AND_03_Overview.php

--- src/courses/CMPP-264/Android/AND_03_Overview.php
+++ src/courses/CMPP-264/Android/AND_03_Overview.php
@@ -30,7 +30,23 @@ MAINHEADING;
       $returnValue = '
 
 <h2>Activity</h2>
-<p></p>
+<p>
+  A single, focused thing that the user can do. Almost all activities interact with the user, so the Activity class 
+  takes care of creating a window for you in which you can place your UI with <code>setContentView(View)</code>
+</p>
+<p>
+  Initialize the activity in <code>onCreate(Bundle)</code>. This is where you will usually call 
+  <code>setContentView(int)</code> with a layout resource defined in the manifest, and use <code>findViewById(int)</code>
+  to retrieve the widgets that you need to interact with programmatically
+</p>
+<p>
+  <code>onPause()</code> is where you deal with the user pausing active interaction with the activity. Any changes made 
+  by the user should at this point be committed - usually to the <code>ContentProvider</code> holding the data.
+</p>
+<p>
+  To be of use with <code>Context.startActivity()</code>, all activity classes must hava a corresponding 
+  <code>&lt;activity&gt;</code> declaration in their package\'s <code>AndroidManifest.xml</code>
+</p>
 <hr>
 
 <h2>Create an icon</h2>

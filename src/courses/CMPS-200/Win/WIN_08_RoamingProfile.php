817ad3889af15df908108da8dd1b622be78fa2b9
zorkwarrior <zorkwarrior@gmail.com>
2019-11-11 12:27:56 -0700

    Update WIN_08_RoamingProfile.php

--- src/courses/CMPS-200/Win/WIN_08_RoamingProfile.php
+++ src/courses/CMPS-200/Win/WIN_08_RoamingProfile.php
@@ -28,7 +28,16 @@ MAINHEADING;
     public function getContent() : string
     {
       $returnValue = '
-<h2>Intro</h2>
+<h2>Create a Roaming Directory</h2>
+<ol>
+  <li>Open the <kbd>Network Drives</kbd> partition and create a new folder called <b>Roaming</b></li>
+  <li>Right-click the new folder and select <kbd>Share with</kbd> -> <kbd>Specific people...</kbd></li>
+  <li>
+    Type in the <b>Group</b> or <b>User</b> you would like to grant access to this folder and click <kbd>Add</kbd><br>
+    Set their <kbd>Permission Level</kbd> to <b>Read/Write</b><br>    
+  </li>
+  <li>Repeat as necessary and click <kbd>Share</kbd></li>
+</ol>
 
 
 ';
